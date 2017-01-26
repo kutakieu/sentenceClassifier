8af4cc54803e7b59c0865b775840251ca754b1ac
kutakieu <kutakieu22@gmail.com>
2017-01-26 12:21:00 +0900

    added word similarities, H, p sentence index as new features

--- LSTM_1.py
+++ LSTM_1.py
@@ -15,7 +15,7 @@ import sys, getopt
 from random import shuffle
 import random
 from gensim.models import Word2Vec
-
+import re
 # Import MNIST data
 # from tensorflow.examples.tutorials.mnist import input_data
 # mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
@@ -28,12 +28,12 @@ we will then handle 28 sequences of 28 steps for every sample.
 
 # Parameters
 learning_rate = 0.001
-training_iters = 10000
+training_iters = 20000
 batch_size = 50
 display_step = 10
 num_data = 0
 # Network Parameters
-n_input = 200 # MNIST data input (img shape: 28*28)
+n_input = 206 # MNIST data input (img shape: 28*28)
 n_steps = 60 # timesteps
 n_hidden = 200 # hidden layer num of features
 n_classes = 2 # MNIST total classes (0-9 digits)
@@ -55,33 +55,58 @@ fin = open("/Users/tAku/Nextremer/data/wikidata_pNdd_sameTopicANDsameSection2/20
 data_path = "/Users/tAku/Nextremer/data/"
 model = Word2Vec.load_word2vec_format(data_path + 'foodVector.bin', binary=True)
 
+file_id2food = open("/Users/tAku/Nextremer/data/wikidata_pNdd_sameTopicANDsameSection2/id2food.txt", encoding="utf-8")
+id2food = {}
+remove_squareBracket = re.compile(r'\[.*?\]|（.*?）| \(.*?\)')
 def prepare():
+    """
+    construct the id2food dictionary
+    """
+    lines = file_id2food.readlines()
+    for line in lines:
+        line = line[:-1]
+        line = line.split(" ")
+        _id = line[0]
+        food = "".join(line[1:])
+        id2food[int(_id)] = food
+    print("complete making id2food dictionary")
+
     lines = fin.readlines()
     shuffle(lines)
-    max_length = 60
-    unknown = np.zeros(200)
+    max_length = n_steps
+    unknown = np.zeros(n_input)
     num_data = len(lines)
-    data = np.zeros((len(lines),60,200))
+    data = np.zeros((len(lines),n_steps,n_input))
     labels = []
     # random.seed(0)
-    j = 0
+    j = 0   #indenx of sentences (= 0~num_of_labeled_sentences)
+    # print(id2food[10])
     for line in lines:
         line = line.split(",")
+        _id = line[0]
+        food_id = int(_id[0:len(_id)-7])
+        h_sort = int(_id[-7])
+        h_id = int(_id[len(_id)-6:len(_id)-4])
+        p_id = int(_id[len(_id)-4:len(_id)-2])
+        s_id = int(_id[len(_id)-2:len(_id)])
+        topic = remove_squareBracket.sub("",id2food[food_id])
         label = line[1]
         onehot = [1,0] if label == "t" else [0,1]
         labels.append(onehot)
 
         morphemes = line[2].split(" ")
         morphemes = morphemes[:-1]
+        sentence_length = len(morphemes)
+
         if len(morphemes) < max_length:
             while len(morphemes) < max_length:
                 morphemes.insert(0, "<unknown>")
-        elif len(morphemes) > max_length: 
+        elif len(morphemes) > max_length:
             morphemes = morphemes[0:max_length]
 
         # fout.write(line[1] + ";")
         wordVecs = np.zeros((n_steps,n_input))
-        i = 0
+        i = 0   #index of term/word of each sentence. range is (0~59)
         for morpheme in morphemes:
             try:
                 score = model[morpheme]
@@ -89,13 +114,23 @@ def prepare():
                 """
                 ここから各単語とトピックの類似度をパラメーターの201番目として渡す
                 """
-                # wordVecs[i][200] = model.similarity(topic,morpheme)
+                wordVecs[i][200] = model.similarity(topic,morpheme)
             except:
                 wordVecs[i] = unknown
                 # wordVecs[i][200] = unknown
+            wordVecs[i][201] = h_sort
+            wordVecs[i][202] = h_id
+            wordVecs[i][203] = p_id
+            wordVecs[i][204] = s_id
+            wordVecs[i][205] = sentence_length
             i += 1
         data[j] = wordVecs
         j += 1
+        # print(str(j) + " files complete vectorizing")
+        if j % 100 == 0:
+            print(str(j) + " files complete vectorizing")
+    print("complete vectorize data set")
+
     return np.asarray(data[:1000]), np.asarray(data[1000:]), np.asarray(labels[:1000]), np.asarray(labels[1000:])
 
 def get_next_batch(x, y, index):
